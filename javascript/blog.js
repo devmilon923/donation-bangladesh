83465e5bef556d02b5912c908efa2a277928204c
MD Milon <milon@kali>
2024-09-23 14:18:23 +0600

    add some change in blog and js file

---
+++ javascript/blog.js
@@ -0,0 +1,18 @@
+document.getElementById("toggle-icon").addEventListener("click", function () {
+    document.getElementById("mobile-manu").classList.toggle("hidden");
+  });
+  
+  const dt = document.querySelectorAll("dt");
+  for (let element of dt) {
+    // console.log(element)
+    element.addEventListener("click", function () {
+      const dd = element.getAttribute("aria-controls");
+      // console.log(dd)
+      const ddElement = document.getElementById(dd);
+      // console.log(ddElement)
+      const icon = element.querySelectorAll("i")[0];
+  
+      ddElement.classList.toggle("hidden");
+      icon.classList.toggle("-rotate-180");
+    });
+  }
--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,21 @@
-document.getElementById("toggle-icon").addEventListener("click", function () {
-  document.getElementById("mobile-manu").classList.toggle("hidden");
+
+
+const historyButton = document.getElementById("history-button");
+const donationButton = document.getElementById("donation-button");
+const donationTab = document.getElementById("donation-tab");
+const historyTab = document.getElementById("history-tab");
+
+historyButton.addEventListener("click", function () {
+  historyButton.classList.add("bg-lime-300", "focus:bg-lime-300");
+  donationButton.classList.remove("bg-lime-300", "focus:bg-lime-300");
+
+  historyTab.classList.remove('hidden')
+  donationTab.classList.add('hidden')
 });
+donationButton.addEventListener("click", function () {
+  historyButton.classList.remove("bg-lime-300", "focus:bg-lime-300");
 
-const dt = document.querySelectorAll("dt");
-for (let element of dt) {
-  // console.log(element)
-  element.addEventListener("click", function () {
-    const dd = element.getAttribute("aria-controls");
-    // console.log(dd)
-    const ddElement = document.getElementById(dd);
-    // console.log(ddElement)
-    const icon = element.querySelectorAll("i")[0];
-
-    ddElement.classList.toggle("hidden");
-    icon.classList.toggle("-rotate-180");
-  });
-}
+  donationButton.classList.add("bg-lime-300", "focus:bg-lime-300");
+  historyTab.classList.add('hidden')
+  donationTab.classList.remove('hidden')
+});
